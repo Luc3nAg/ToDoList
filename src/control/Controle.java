1940680b2db731b16001b9b4f039d6327600318e
Lunare-dot <matheusalizsouza@gmail.com>
2025-09-09 19:23:13 -0300

    Adição função 'listarTarefas' + correção2.0prime

--- src/control/Controle.java
+++ src/control/Controle.java
@@ -26,17 +26,26 @@ public class Controle {
 		
 		System.out.println("Tarefa adicionada: " + tarefa);
 	}
-	
 	public void editarTarefa() {
 		System.out.println("EDITAR TAREFA");
 	}
-	
 	public void listarTarefa() {
-		System.out.println("LISTAR TAREFA");
+		System.out.println("\nLISTAR TAREFAS");
+		
+		if (Lista.isEmpty()) System.out.println("Nenhuma tarefa registrada!\n");
+		else {
+			Iterator<Tarefas> listar = Lista.iterator();
+			while (listar.hasNext()){
+				Tarefas tarefa = listar.next();
+				System.out.println("======================");
+				System.out.println("ID: " + tarefa.getId());
+				System.out.println("Título: " + tarefa.getTitulo());
+				System.out.println("Descrição: " + tarefa.getDescrição());
+				System.out.println("======================\n");
+			}
+		}
 	}
-	
 	public void excluirTarefa() {
 		System.out.println("EXCLUIR TAREFA");
 	}
-	
 }
